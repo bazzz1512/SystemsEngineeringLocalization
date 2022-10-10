6d63766eb483497888e300625135b147baf77417
Sebastian <bazzz1512@users.noreply.github.com>
2022-10-10 17:00:56 +0200

    Whatever

--- src/cLocalization.py
+++ src/cLocalization.py
@@ -1,14 +1,19 @@
 import numpy as np
+import math
+
 
 class cLocalization:
     def __init__(self, nodes, pos):
         self.nodes = nodes
         self.pos = pos
-        self.distances_to_nodes = None
+        self.distances_to_nodes = []
 
     def calculate_dis(self):
         # TODO: Calculate position from different points and create localization from that
-        pass
+        self.distances_to_nodes = []
+        for i in self.nodes:
+            distance = math.sqrt((i[0] - self.pos[0]) ** 2 + (i[1] - self.pos[1]) ** 2)
+            self.distances_to_nodes.append(distance)
 
     def __str__(self):
         return_string = f""
@@ -16,5 +21,8 @@ class cLocalization:
         for idx, val in enumerate(self.nodes):
             return_string += f"Node {idx}: {val[0]}, {val[1]}\n"
         return_string += f"Pos: {self.pos[0]}, {self.pos[1]}\n"
-        return return_string
+        return_string += f"Current distances: \n"
+        for i in self.distances_to_nodes:
+            return_string += f"    {i}\n"
 
+        return return_string
--- src/main.py
+++ src/main.py
@@ -4,5 +4,6 @@ pos = [0,0]
 nodes_list = [[1,1],[0,0],[1,3], [5,2], [-1, 6]]
 
 instance = cLocalization(nodes_list, pos)
+instance.calculate_dis()
 
 print(instance)
