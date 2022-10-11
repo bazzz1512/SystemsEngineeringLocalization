6aeef35f1427aa20cd855ed1b9d056e90a38b142
crwijnveen <c.r.wijnveen@student.tudelft.nl>
2022-10-11 11:38:36 +0200

    Basis for triangulation added

--- src/cLocalization.py
+++ src/cLocalization.py
@@ -17,6 +17,44 @@ class cLocalization:
             distance = math.sqrt((i[0] - self.pos[0]) ** 2 + (i[1] - self.pos[1]) ** 2)
             self.distances_to_nodes.append(distance)
 
+    def triangulate(self):
+        # Get the x-y pairs of circles around the nodes
+        # All circles should have an x-y pair that matches up (within an error_margin)
+        # This pair is where the signal is sent from
+        # Use fill_circle_vals to get the x-y pairs of a circle
+        return 0
+
+    @staticmethod
+    def fill_circle_vals(distance, position, n): #Position is node position
+        #Circle equation: (x-pos(x))^2 + (y-pos(y))^2 = distance^2
+        print(distance)
+        x_and_y = []
+        x_pos, y_pos = position[0], position[1]
+        for x in np.linspace(-distance+x_pos, distance+x_pos, n):
+            print(x)
+            c = y_pos**2 + x**2 - distance**2 -2*x_pos*x + x_pos**2
+            y1, y2, sols = cLocalization.quadratic_function(1, -2*y_pos, c)
+            match sols:
+                case 1:
+                    x_and_y.append((x, y1))
+                case 2:
+                    x_and_y.append((x, y1))
+                    x_and_y.append((x, y2))
+                case _:
+                    print("Something went wrong in fill_circle_vals()")
+        return x_and_y
+            
+    @staticmethod
+    def quadratic_function(a, b, c):
+        try:
+            discriminant = math.sqrt(b**2 - 4*a*c)
+            if discriminant == 0:
+                return (-b/(2*a), 0, 1)
+            else:
+                return ((-b+discriminant)/(2*a), (-b-discriminant)/(2*a), 2)
+        except ValueError:
+            print("Imaginary root, invalid node?")
+    
     def add_noise_to_dis(self, abs_error):
         for idx, huts in enumerate(self.distances_to_nodes):
             self.distances_to_nodes[idx] += (random.random() * 2 -1) * abs_error
