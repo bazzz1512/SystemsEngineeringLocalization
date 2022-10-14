2e7dfa4f7dc6583748bc4eea0f54308499541d50
crwijnveen <c.r.wijnveen@student.tudelft.nl>
2022-10-14 07:47:20 +0200

    Faster program speed, mode doesn't work yet

--- src/cLocalization.py
+++ src/cLocalization.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import math
 import random
 from scipy import stats as st
+from shapely.geometry import LineString
 
 
 class cLocalization:
@@ -19,52 +20,55 @@ class cLocalization:
             distance = math.sqrt((i[0] - self.pos[0]) ** 2 + (i[1] - self.pos[1]) ** 2)
             self.distances_to_nodes.append(distance)
 
-    def triangulate(self, n, error_margin):
+    def triangulate(self, n):
         # Get the x-y pairs of circles around the nodes
         # All circles should have an x-y pair that matches up (within an error_margin)
         # This pair is where the signal is sent from
         # Use fill_circle_vals to get the x-y pairs of a circle
         possible_points = []
-        # Use the first 2 circles to build a basis for circle intercepts
-        points1 = cLocalization.fill_circle_vals(self.distances_to_nodes[0], self.nodes[0], n)
-        points2 = cLocalization.fill_circle_vals(self.distances_to_nodes[1], self.nodes[1], n)
-        for i in range(len(points2)):
-            for j in range(len(points1)):
-                coord1 = points1[j]
-                coord2 = points2[i]
-                if cLocalization.is_equal(coord1, coord2, error_margin):
-                    possible_points.append(coord1)
-        for ndx in range(2, len(self.nodes)):
-            points = cLocalization.fill_circle_vals(self.distances_to_nodes[ndx], self.nodes[ndx], n)
-            for i, _ in enumerate(possible_points):
-                match = False
-                for j in range(len(points)):
-                    if cLocalization.is_equal(possible_points[i], points[j], error_margin):
-                        match = True
-                if not match:
-                    possible_points.pop(i)
-        est_pos = st.mode(possible_points).mode
-        if len(est_pos) == 0:
-            if error_margin > 0.05:
-                print("Can't find an intersection, returning.")
-                return [[0, 0]]
-            print("Couldn't find an intersection, trying again with larger error margin: " + str(error_margin) + "->" + str(error_margin + 0.01))
-            est_pos = self.triangulate(n, error_margin + 0.01)
-        return est_pos
+        for ndx in range(1, len(self.nodes)):
+            x_1, y_1 = cLocalization.fill_circle_vals(self.distances_to_nodes[ndx-1], self.nodes[ndx-1], n)
+            x_2, y_2 = cLocalization.fill_circle_vals(self.distances_to_nodes[ndx], self.nodes[ndx], n)
+            first_line = LineString(np.column_stack((x_1, y_1)))
+            second_line = LineString(np.column_stack((x_2, y_2)))
+            intersection = first_line.intersection(second_line)
+            if intersection.geom_type == 'MultiPoint':
+                x, y = LineString(np.asarray(intersection)).xy
+                possible_points += cLocalization.tuple_coord_from_list(x, y)
+            elif intersection.geom_type == 'Point':
+                possible_points.append(intersection.xy)
+            else:
+                print("User out of bounds, intersection type: ", str(intersection.geom_type))
+                return [0, 0]
+        #mode doesn't work on noisy tuples, need to find another way to extract which tuple is most common with a tolerance
+        return st.mode(possible_points).mode[0]
 
     @staticmethod
-    def is_equal(coord1, coord2, error_mar):
-        x_ref, y_ref = coord1
-        x_test, y_test = coord2
-        x_y_pass = [False, False]
-        if x_ref - error_mar <= x_test <= x_ref + error_mar:
-            x_y_pass[0] = True
-        if y_ref - error_mar <= y_test <= y_ref + error_mar:
-            x_y_pass[1] = True
-        if all(x_y_pass):
-            return True
-        else:
-            return False
+    def fill_circle_vals(distance, position, n): #Position is node position
+        #Circle equation: (x-pos(x))^2 + (y-pos(y))^2 = distance^2
+        step = 2*math.pi/n
+        a = position[0]
+        b = position[1]
+        r = distance
+        x = []
+        y = []
+        theta = 0
+        while theta < 2*math.pi:
+            x.append(r * math.cos(theta) + a)
+            y.append(r * math.sin(theta) + b)
+            theta += step
+        return x, y
+            
+    @staticmethod
+    def tuple_coord_from_list(x, y):
+        coords = []
+        for i in range(len(x)):
+            coords.append((x[i], y[i]))
+        return coords
+    
+    def add_noise_to_dis(self, abs_error):
+        for idx, huts in enumerate(self.distances_to_nodes):
+            self.distances_to_nodes[idx] += (random.random() * 2 -1) * abs_error
 
     @staticmethod
     def split_coords(coords):
@@ -74,25 +78,6 @@ class cLocalization:
             x.append(coord[0])
             y.append(coord[1])
         return x, y
-
-    @staticmethod
-    def fill_circle_vals(distance, position, n): #Position is node position
-        #Circle equation: (x-pos(x))^2 + (y-pos(y))^2 = distance^2
-        step = 2*math.pi/n
-        a = position[0]
-        b = position[1]
-        r = distance
-        coords = []
-        theta = 0
-        while theta < 2*math.pi:
-            coords.append((r * math.cos(theta) + a, r * math.sin(theta) + b))
-            theta += step
-        return coords
-            
-    
-    def add_noise_to_dis(self, abs_error):
-        for idx, huts in enumerate(self.distances_to_nodes):
-            self.distances_to_nodes[idx] += (random.random() * 2 -1) * abs_error
 
     def __str__(self):
         return_string = f""
@@ -110,7 +95,7 @@ class cLocalization:
         for node in self.nodes:
             plt.plot(node[0], node[1], color='green', marker='s')
         plt.plot(self.pos[0], self.pos[1], 'o')
-        estim = self.triangulate(1500, 0.01)[0]
+        estim = self.triangulate(100)
         plt.plot(estim[0], estim[1], '^')
         plt.title("Localization Area")
         plt.show()
@@ -122,7 +107,7 @@ class cLocalization:
             self.pos = coord
             self.calculate_dis()
             self.add_noise_to_dis(0.005)
-            est_path.append(self.triangulate(1500, 0.01)[0])
+            est_path.append(self.triangulate(50))
             print(str(100*(i+1)/len(path)) + "%")
         for node in self.nodes:
             plt.plot(node[0], node[1],  color='green', marker='s')
